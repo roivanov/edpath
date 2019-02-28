0cb94251abbb46ad4d0eb8d423b229b74f303b0f
Roman Ivanov <me@roivanov.com>
2019-02-28 08:16:06 -0800

    first run

--- edpath.py
+++ edpath.py
@@ -14,11 +14,6 @@ from collections import OrderedDict, namedtuple
 
 import requests
 
-SYSTEMS = ['Great Annihilator',
-           'Hypoe Flyi HW-W e1-7966',
-           'HYPOE FLYI HX-T E3-295',
-           'STUEMEAE KM-W C1-342']
-
 API_CALL = 'https://www.edsm.net/api-v1/system'
 DELAY = 3
 CACHE_DIR = '.edpathcache'
@@ -32,14 +27,48 @@ class Coords(namedtuple('Coords', 'x y z')):
                               math.pow(self.y - other.y, 2),
                               math.pow(self.z - other.z, 2)]))
 
+class System(namedtuple('System', 'name alias')):
+    """System we travel"""
+    pass
+
+class PathTo(object):
+    """Compute path"""
+    def __init__(self, path):
+        self._path = path
+
+    @property
+    def length(self):
+        """Compute distance on the path"""
+        return sum([self._path[_i].distance_to(self._path[_i + 1])
+                    for _i in range(len(self._path) - 1)])
+
+SYSTEMS = [System('Great Annihilator', 'Great Annihilator'),
+
+           System('Hypoe Flyi HW-W e1-7966', 'Galionas'),
+           System('HYPOE FLYI HX-T E3-295', 'Caeruleum Luna "Mysturji Crater"'),
+           System('Zunuae HL-Y e6903', 'Zunuae Nebula'),
+           System('Byoomao MI-S e4-5423', 'Wulfric'),
+           System('Sagittarius A*', 'Sagittarius A*'),
+
+           System('Kyli Flyuae WO-A f39', 'Dance of the Compact Quartet'),
+           System('Myriesly DQ-G d10-1240', 'Insinnergy\'s World'),
+           System('Myriesly RY-S e3-5414', 'Six Rings'),
+           System('Myriesly CL-P e5-4186', 'Emerald Remnant'),
+           System('Myriesly CL-P e5-7383', 'Fenrisulfur'),
+
+           System('STUEMEAE KM-W C1-342', 'WP7 Altum Sagittarii'),
+          ]
+
 if __name__ == '__main__':
     if not os.path.exists(CACHE_DIR):
         os.mkdir(CACHE_DIR)
 
     all_systems = {}
+    aliases = {}
 
     # load data
-    for system in SYSTEMS:
+    for each in SYSTEMS:
+        system = each.name
         print(system)
         _hash = hashlib.sha256(system.encode('utf-8')).hexdigest()
         base_dir = os.path.join(CACHE_DIR, _hash[0])
@@ -73,30 +102,32 @@ if __name__ == '__main__':
         coords = Coords(**data['coords'])
         print(coords)
         all_systems[system] = coords
+        aliases[system] = each.alias
 
+    print('ALL SYSTEMS:')
     print(all_systems)
 
     # firect path from A to Z
-    direct_path = all_systems[SYSTEMS[0]].distance_to(all_systems[SYSTEMS[-1]])
-    print('Direct path is %s', direct_path)
-
-    # path in the order of systems
-    best_order = SYSTEMS
-    best_path = sum([all_systems[SYSTEMS[i]].distance_to(all_systems[SYSTEMS[i+1]]) for i in range(len(SYSTEMS) - 1)])
-    print('First path is %s', best_path)
+    original_order = [x.name for x in SYSTEMS]
+    start_point = all_systems[original_order[0]]
+    final_point = all_systems[original_order[-1]]
+    direct_path = PathTo([start_point, final_point])
+    print('Direct path is %s', direct_path.length)
 
     # all permutations
-    i = itertools.permutations(SYSTEMS[1:-1])
+    best_order = None
+    best_len = 0
+    i = itertools.permutations(original_order[1:-1])
     for each in i:
         print(each)
-        perm_list = [SYSTEMS[0]] + list(each) + [SYSTEMS[-1]]
-        print(perm_list)
-        curr_path = sum([all_systems[perm_list[i]].distance_to(all_systems[perm_list[i+1]]) for i in range(len(perm_list) - 1)])
-        print('Current path is %s', curr_path)
-        if curr_path < best_path:
-            best_path = curr_path
-            best_order = perm_list
-    
+        perm_list = [start_point] + list([all_systems[x] for x in each]) + [final_point]
+        curr_path = PathTo(perm_list)
+        curr_len = curr_path.length
+        print('Current path is %s', curr_len)
+        if best_len == 0 or curr_len < best_len:
+            best_len = curr_len
+            best_order = each
+
     print('-' * 60)
-    print(best_path)
-    print(best_order)
+    print(best_len)
+    print(original_order[0], [aliases[x] for x in best_order], original_order[-1])
