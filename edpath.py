9179e975cb7b515d7db81e723fe24f3ab8c23285
Roman Ivanov <me@roivanov.com>
2019-03-01 17:18:45 -0800

    refactor

--- edpath.py
+++ edpath.py
@@ -3,71 +3,20 @@ Find shortest path between two systems while visiting all POI in between
 """
 from __future__ import print_function, unicode_literals
 
-import hashlib
 import json
 import math
 import random
-import os
 import time
 import urllib
-from contextlib import contextmanager
 from collections import OrderedDict
 
 import requests
 
+from filecache import FileCache
+
 API_CALL = 'https://www.edsm.net/api-v1/system'
 # be polite, delay in seconds
 DELAY = 3
-
-class FileCache(object):
-    """File Cache of the objects or data"""
-
-    # cache directory
-    CACHE_DIR = '.edpathcache'
-    # number of chracters used to build cache tree
-    _DIR_CHAR = 2
-
-    if not os.path.exists(CACHE_DIR):
-        os.mkdir(CACHE_DIR)
-
-    def __init__(self, utf8_encoded_name):
-        self.__fname = FileCache.set_fname(utf8_encoded_name)
-        print(self.__fname)
-        raise NotImplementedError
-
-    @staticmethod
-    def set_fname(utf8_encoded_name):
-        _hash = hashlib.sha256(utf8_encoded_name).hexdigest()
-        base_dir = os.path.join(FileCache.CACHE_DIR, _hash[:FileCache._DIR_CHAR])
-        if not os.path.exists(base_dir):
-            os.mkdir(base_dir)
-
-        return os.path.join(base_dir, _hash[FileCache._DIR_CHAR:])
-
-    @property
-    def fname(self):
-        return self.__fname
-
-    @fname.setter
-    def fname(self, value):
-        self.__fname = FileCache.set_fname(value)
-
-    def save(self, data):
-        with open(self.fname, 'w') as f:
-            f.write(data)
-
-    @contextmanager
-    def open(self):
-        if os.path.exists(self.fname):
-            # print('Loading from file')
-            with open(self.fname) as f:
-                yield f
-        else:
-            yield None
-
-    def remove(self):
-        os.remove(self.fname)
-
 
 class Coords(object):
     """XYZ coordinates"""
---
+++ filecache.py
@@ -0,0 +1,55 @@
+from __future__ import print_function
+
+import hashlib
+import os
+from contextlib import contextmanager
+
+
+class FileCache(object):
+    """File Cache of the objects or data"""
+
+    # cache directory
+    CACHE_DIR = '.edpathcache'
+    # number of chracters used to build cache tree
+    _DIR_CHAR = 2
+
+    if not os.path.exists(CACHE_DIR):
+        os.mkdir(CACHE_DIR)
+
+    def __init__(self, utf8_encoded_name):
+        self.__fname = FileCache.set_fname(utf8_encoded_name)
+        print(self.__fname)
+        raise NotImplementedError
+
+    @staticmethod
+    def set_fname(utf8_encoded_name):
+        _hash = hashlib.sha256(utf8_encoded_name).hexdigest()
+        base_dir = os.path.join(FileCache.CACHE_DIR, _hash[:FileCache._DIR_CHAR])
+        if not os.path.exists(base_dir):
+            os.mkdir(base_dir)
+
+        return os.path.join(base_dir, _hash[FileCache._DIR_CHAR:])
+
+    @property
+    def fname(self):
+        return self.__fname
+
+    @fname.setter
+    def fname(self, value):
+        self.__fname = FileCache.set_fname(value)
+
+    def save(self, data):
+        with open(self.fname, 'w') as f:
+            f.write(data)
+
+    @contextmanager
+    def open(self):
+        if os.path.exists(self.fname):
+            # print('Loading from file')
+            with open(self.fname) as f:
+                yield f
+        else:
+            yield None
+
+    def remove(self):
+        os.remove(self.fname)
